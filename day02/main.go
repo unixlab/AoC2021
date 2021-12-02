d6ac7369293372f5f5563ec38eaf585c5b7e2cf9
Felix Hillingshaeuser <felix@mxcore.de>
2021-12-02 08:07:32 +0100

    add day02 part 2

--- day02/main.go
+++ day02/main.go
@@ -17,21 +17,28 @@ func Run() {
 	scanner := bufio.NewScanner(file)
 
 	horizontal := 0
-	depth := 0
+	depthP1 := 0
+
+	aim := 0
+	depthP2 := 0
 
 	for scanner.Scan() {
 		increment, _ := strconv.Atoi(strings.Split(scanner.Text(), " ")[1])
 		switch strings.Split(scanner.Text(), " ")[0] {
 		case "forward":
 			horizontal += increment
+			depthP2 += aim * increment
 		case "up":
-			depth -= increment
+			depthP1 -= increment
+			aim -= increment
 		case "down":
-			depth += increment
+			depthP1 += increment
+			aim += increment
 		default:
 			panic("invalid input")
 		}
 	}
 
-	fmt.Printf("part 1 => %d\n", horizontal*depth)
+	fmt.Printf("part 1 => %d\n", horizontal*depthP1)
+	fmt.Printf("part 2 => %d\n", horizontal*depthP2)
 }
